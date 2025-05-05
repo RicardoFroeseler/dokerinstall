5404b58428ed5908abb21f3896e0cee04a400871
Ricardo Froeseler <42811079+RicardoFroeseler@users.noreply.github.com>
2025-05-05 15:39:34 -0300

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const btn = document.createElement('a');
+    btn.id = 'materprint-support';
+    btn.href = 'https://materprint.com.br';
+    btn.target = '_blank';
+    btn.innerText = 'Suporte MaterPrint';
+    document.body.appendChild(btn);
+  });
+  
